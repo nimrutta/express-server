b12530195c05efce8915e615a09ea77073c443a5
Nimrutta <nimrutta@gmail.com>
2021-12-13 14:02:33 +0300

    Connect mongo db cluster to server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require( 'express' )
 const app     = express()
 const port    = process.env.PORT || 3000
+const mongoose = require("mongoose");
+
 app.get( '/' ,(req, res) => {
 res.type( 'text/plain' )
 res.send( 'Server Expresso ☕' )
@@ -14,5 +16,10 @@ res.type( 'text/plain' )
 res.status( 404 )
 res.send('404 Not found ☕_☕')
 })
+
+mongoose.connect("mongodb+srv://Nimrutta:4DpFDXLeFGaUkTlV@clusterd.64lgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+.then(() => console.log("Database connection successful"))
+.catch(() => console.log("Database connection error"));
+
 app.listen( port ,
 () => console.log(`Expresso ☕ is on Port ${ port } Ctrl + C to Stop `) )
